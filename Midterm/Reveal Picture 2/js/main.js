6ecef662d4d1cfb252d9e3aa73bd83b757a2c59e
ClariiD <clarisadiaz@gmail.com>
2013-10-10 01:15:23 -0400

    js variables, reveal function

---
+++ Midterm/Reveal Picture 2/js/main.js
@@ -0,0 +1,19 @@
+// document.querySelector(".outline").addEventListener("click", function(){
+//     document.querySelector(".picture").style.display = "block";
+//     document.querySelector(".picture").style.opacity = "1s";
+// });
+
+var fade_in_element = document.getElementById('.picture'),
+	fade_out_element = document.getElementById('.outline');
+
+
+querySelector.('.picture').addEventListener('click', function() {
+    alert('Hello world');
+}, false);
+
+function reveal{
+	document.getElementById('.picture').addclass('.fade-in', false);
+}
+
+reveal();
+
