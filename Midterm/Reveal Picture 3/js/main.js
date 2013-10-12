e1e17074375dfc5a463ad06c09c622181c439c6c
ClariiD <clarisadiaz@gmail.com>
2013-10-12 13:24:53 -0400

    function, event listener, load

--- Midterm/Reveal Picture 3/js/main.js
+++ Midterm/Reveal Picture 3/js/main.js
@@ -18,28 +18,47 @@
 // reveal();
 
 
-$(document).ready(function(){
-    var animals=[{id: "outline_dog", image: "odog.png", layer: 1, fade: "fade_out"},
-              {id: "picture_dog", image: "pdog.png", layer: 2, fade: "fade_in"}, 
-              {id: "outline_zebra", image: "ozebra.png", layer: 1, fade: "fade_out"},
-              {id: "picture-zebra", image: "pzebra.png", layer: 2, fade: "fade_in"},
-              {id: "outline_lion", image: "olion.png", layer: 1, fade: "fade-out"}, 
-              {id: "picture-lion", image: "plion.png", layer: 2, fade: "fade-in"},
-              {id: "outline-elephant", image: "oelephant.png", layer: 1, fade: "fade-out"},
-              {id: "picture-elephant", image: "pelephant.png", layer: 2, fade: "fade-in"},
-              {id: "outline-owl", image: "oowl.png", layer: 1, fade: "fade-out"},
-              {id: "picture-owl", image: "powl.png", layer: 2, fade: "fade-in"},
-              {id: "outline-pig", image: "opig.png", layer: 1, fade: "fade-out"},
-              {id: "picture-pig", image: "ppig.png", layer: 2, fade: "fade-in"}, 
-              {id: "outline-sheep", image: "osheep.png", layer: 1, fade: "fade-out"},
-              {id: "picture_sheep", image: "psheep.png", layer: 2, fade: "fade-in"},];
+// $(document).ready(function(){
+//     var animals=[{id: "outline_dog", image: "odog.png", layer: 1, fade: "fade_out"},
+//               {id: "picture_dog", image: "pdog.png", layer: 2, fade: "fade_in"}, 
+//               {id: "outline_zebra", image: "ozebra.png", layer: 1, fade: "fade_out"},
+//               {id: "picture-zebra", image: "pzebra.png", layer: 2, fade: "fade_in"},
+//               {id: "outline_lion", image: "olion.png", layer: 1, fade: "fade-out"}, 
+//               {id: "picture-lion", image: "plion.png", layer: 2, fade: "fade-in"},
+//               {id: "outline-elephant", image: "oelephant.png", layer: 1, fade: "fade-out"},
+//               {id: "picture-elephant", image: "pelephant.png", layer: 2, fade: "fade-in"},
+//               {id: "outline-owl", image: "oowl.png", layer: 1, fade: "fade-out"},
+//               {id: "picture-owl", image: "powl.png", layer: 2, fade: "fade-in"},
+//               {id: "outline-pig", image: "opig.png", layer: 1, fade: "fade-out"},
+//               {id: "picture-pig", image: "ppig.png", layer: 2, fade: "fade-in"}, 
+//               {id: "outline-sheep", image: "osheep.png", layer: 1, fade: "fade-out"},
+//               {id: "picture_sheep", image: "psheep.png", layer: 2, fade: "fade-in"},];
 
-var $fade_in_element = $('.picture'),
-    $fade_out_element = $('.outline');
+// var $fade_in_element = $('.picture'),
+//     $fade_out_element = $('.outline');
 
+// var fade_in_element = document.getElementByClassName('.picture'),
+//     fade_out_element = document.getElementByClassName('.outline');
 
-$('.outline').click(function(){
-	function Reveal(){
-	    $('.picture').fadeIn('slow').addClass('.reveal');
-	};
+function loadReveal(callback){
+  callback();
+}
+
+loadReveal(function() {
+  alert('Reveal');
 });
+
+addEventListener('click',function(evt) {
+  $('.picture').fadeIn('slow').addClass('.reveal');
+    alert('Hello world');
+},false);
+
+
+// $('.outline').click(function(){
+//   function Reveal(){
+//       $('.picture').fadeIn('slow').addClass('.reveal');
+//   };
+//   Reveal();
+// });
+
+
