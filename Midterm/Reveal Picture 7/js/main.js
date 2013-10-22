3b0aa8e047faf48bed81bc6bcfe4790864cfd4cf
ClariiD <clarisadiaz@gmail.com>
2013-10-22 14:33:05 -0400

    vanilla js

---
+++ Midterm/Reveal Picture 7/js/main.js
@@ -0,0 +1,34 @@
+// var animals=[{id: "outline_dog", image: "img/odog.png"},
+//           {id: "picture_dog", image: "img/pdog.png"}, 
+//           {id: "outline_zebra", image: "img/ozebra.png"},
+//           {id: "picture-zebra", image: "img/pzebra.png"},
+//           {id: "outline_lion", image: "img/olion.png"}, 
+//           {id: "picture-lion", image: "img/plion.png"},
+//           {id: "outline-elephant", image: "img/oelephant.png"},
+//           {id: "picture-elephant", image: "img/pelephant.png"},
+//           {id: "outline-owl", image: "img/oowl.png"},
+//           {id: "picture-owl", image: "img/powl.png"},
+//           {id: "outline-pig", image: "img/opig.png"},
+//           {id: "picture-pig", image: "img/ppig.png"}, 
+//           {id: "outline-sheep", image: "img/osheep.png"},
+//           {id: "picture_sheep", image: "img/psheep.png"},];
+
+// var animals = document.querySelector('.animals').getAttribute('.data-user');
+// var myJSON = JSON.parse(animals);
+
+
+function loadReveal(callback){
+  callback();
+}
+
+loadReveal(function() {
+   // alert('Reveal');
+});
+
+addEventListener('click',function(evt) {
+    // alert('Hello world');
+},false);
+
+
+
+
