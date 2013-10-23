66063c98af8479374841163688f6c2ad98ee5246
ClariiD <clarisadiaz@gmail.com>
2013-10-23 18:28:39 -0400

    button &fade

--- Midterm/Reveal Picture 9/js/main.js
+++ Midterm/Reveal Picture 9/js/main.js
@@ -1,30 +1,32 @@
-var global_place=0;
-var pics=[{"id": "outline_dog", "image": "img/1o.png"},
-          	 {"id": "picture_dog", "image": "img/1n.png"},
-          	 {"id": "picture_dog", "image": "img/2o.png"},
-          	 {"id": "picture_dog", "image": "img/2n.png"},
-          	 {"id": "picture_dog", "image": "img/3o.png"},
-          	 {"id": "picture_dog", "image": "img/3n.png"},
-          	 {"id": "picture_dog", "image": "img/4o.png"},
-          	 {"id": "picture_dog", "image": "img/4n.png"},
-          	 {"id": "picture_dog", "image": "img/5o.png"},
-          	 {"id": "picture_dog", "image": "img/5n.png"}];
+// $(document).ready(function(){
+  var global_place=0;
+  var pics=[{"id": "outline_dog", "image": "img/1o.png"},
+            	 {"id": "picture_dog", "image": "img/1n.png"},
+            	 {"id": "picture_dog", "image": "img/2o.png"},
+            	 {"id": "picture_dog", "image": "img/2n.png"},
+            	 {"id": "picture_dog", "image": "img/3o.png"},
+            	 {"id": "picture_dog", "image": "img/3n.png"},
+            	 {"id": "picture_dog", "image": "img/4o.png"},
+            	 {"id": "picture_dog", "image": "img/4n.png"},
+            	 {"id": "picture_dog", "image": "img/5o.png"},
+            	 {"id": "picture_dog", "image": "img/5n.png"}];
 
-function loadReveal(callback){
-  callback();
-}
+  function loadReveal(callback){
+    callback();
+  }
 
-loadReveal(function() {
-   // alert('Reveal');
-});
+  loadReveal(function() {
+     // alert('Reveal');
+     var r = document.getElementById('before').style;
+  });
 
-addEventListener('click',function(evt) {
-	 document.getElementById('before').src = pics[global_place].image;
-	 global_place = (global_place + 1) % pics.length;
-	 console.log(global_place);
-    // alert('Hello world');
-},false);
-
-
-
-
+  addEventListener('click',function(evt) {
+     document.getElementById('before').src = pics[global_place].image;
+     global_place = (global_place + 1) % pics.length;
+     console.log(global_place);
+     // $.each(pics, function(index, image) { 
+     //   $( ".fade" ).fadeIn( "slow");
+     //  });
+      // alert('Hello world');
+  },false);
+// });
